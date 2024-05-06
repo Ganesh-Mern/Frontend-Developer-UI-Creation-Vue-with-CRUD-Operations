28325f90340111403960bb8d7d2d5b510c4596ac
Ganesh-Mern <raghabm7@gmail.com>
2024-05-07 00:09:15 +0530

    last

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+const server =jsonServer.create()
+const router=jsonServer.router("./db.json")
+const middlewares=jsonServer.defaults({
+    static:"./build"
+})
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
+const port =process.env.PORT || 5000;
+server.use(middlewares)
+server.use(
+    jsonServer.rewriter({
+        "/api/*":"/$1"
+    })
+)
+server.use(router)
+server.listen(port,()=>{
+    console.log(`server is running ${port}`);
+})
