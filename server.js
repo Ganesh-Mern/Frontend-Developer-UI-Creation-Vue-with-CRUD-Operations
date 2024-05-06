7bd978e8085634f3710768ee9d27c68992a4d6cb
Ganesh-Mern <raghabm7@gmail.com>
2024-05-07 00:12:54 +0530

    commit

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const jsonServer = require('json-server');
-const server =jsonServer.create()
-const router=jsonServer.router("./db.json")
-const middlewares=jsonServer.defaults({
-    static:"./build"
-})
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const middlewares = jsonServer.defaults();
 
-const port =process.env.PORT || 5000;
-server.use(middlewares)
-server.use(
-    jsonServer.rewriter({
-        "/api/*":"/$1"
-    })
-)
-server.use(router)
-server.listen(port,()=>{
-    console.log(`server is running ${port}`);
-})
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
